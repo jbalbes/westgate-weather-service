89b7efab32d88ffdbc82dcd8ed146f15d04ef8e4
Jack Balbes <jack.balbes@oasisdigital.com>
2020-02-23 13:25:50 -0600

    Better formatted help message

--- src/messages.ts
+++ src/messages.ts
@@ -1,9 +1,10 @@
 export const messages = {
   thanks: `Thank you for purchasing the Cassani Collective's Weather Tracking Service.`,
   help: `For information about current weather conditions, type '!weather'`,
-  dmHelp: `Commands specific to this channel:
-           !updateweather: Generates and announces new weather. Happens automatically at 1201 PST on Tuesdays.
-           !weathersay <text>: Weatherbot says <text> in general chat
-           !weatherset <condition> <new value>: Weatherbot sets specified condition to the new value
-           !weatherannounce: Announces the current weather in general chat. Useful after you are done setting the weather with !weatherset`
+  dmHelp: `
+Commands specific to this channel:
+!updateweather: Generates and announces new weather. Happens automatically at 1200AM PST on Tuesdays.
+!weathersay <text>: Weatherbot says <text> in general chat
+!weatherset <condition> <new value>: Weatherbot sets specified condition to the new value
+!weatherannounce: Announces the current weather in general chat. Useful after you are done setting the weather with !weatherset`
 };
